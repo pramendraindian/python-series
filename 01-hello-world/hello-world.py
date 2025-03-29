a153d6b9cfd52ebcf852e854e8e003e8440e907a
pramendraindian <pramendra.indian@gmail.com>
2025-03-29 11:29:57 +0530

    hello world python with numpy installation

---
+++ 01-hello-world/hello-world.py
@@ -0,0 +1,8 @@
+#Code Reference https://code.visualstudio.com/docs/python/python-tutorial
+#pip install numpy
+import numpy as np
+
+msg = "Roll a dice!"
+print(msg)
+print(np.random.randint(1,9))
+print('hello-world')
